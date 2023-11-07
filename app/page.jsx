306c3f5f5d0fe7ae4e034e58a4049e43962c162e
Camilla Westin <camilla@webstin.se>
2023-11-07 19:28:54 +0100

    Styles

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,13 @@ import TicketCard from "./(components)/TicketCard";
 
 const Dashboard = () => {
   return (
-    <main>
-      <TicketCard />
+    <main className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </main>
   );
 };
