1c148e187d253516929cbfdf726eb53bc16bbaa4
Camilla Westin <camilla@webstin.se>
2023-11-07 19:25:29 +0100

    Mocking

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
-  return <main>Dashboard</main>;
+  return (
+    <main>
+      <TicketCard />
+    </main>
+  );
 };
 
 export default Dashboard;
