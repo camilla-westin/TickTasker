b23f652df80437e614bdb3a8a362108929bf30c3
Camilla Westin <camilla@webstin.se>
2023-11-09 13:42:56 +0100

    Filter by category

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState, useEffect } from "react";
 import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
@@ -12,33 +14,64 @@ const getTickets = async () => {
   }
 };
 
-const Dashboard = async () => {
-  const { tickets } = await getTickets();
+const Dashboard = () => {
+  const [tickets, setTickets] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState(null);
+  const [uniqueCategories, setUniqueCategories] = useState([]);
 
-  const uniqueCategories = [
-    ...new Set(tickets?.map(({ category }) => category)),
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTickets();
+
+      const uniqueCategoriesSet = new Set(
+        data.tickets.map(({ category }) => category)
+      );
+      const uniqueCategories = Array.from(uniqueCategoriesSet);
+
+      setTickets(data.tickets || []);
+      setUniqueCategories(uniqueCategories);
+    };
+
+    fetchData();
+  }, []);
+
+  function handleCatFilter(cat) {
+    setCategoryFilter(cat);
+  }
+
+  const filteredTickets = categoryFilter
+    ? tickets.filter((ticket) => ticket.category === categoryFilter)
+    : tickets;
 
   return (
     <main className="p-5">
       <div>
-        {tickets &&
-          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h2>{uniqueCategory}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-                {tickets
-                  .filter((ticket) => ticket.category === uniqueCategory)
-                  .map((filteredTicket, _index) => (
-                    <TicketCard
-                      id={_index}
-                      key={_index}
-                      ticket={filteredTicket}
-                    />
-                  ))}
-              </div>
-            </div>
+        <div className="mb-4 flex justify-end">
+          <button
+            className={`rounded-3xl border border-black border-solid inline-block bg-white mr-2 mb-4 px-3 py-1 ${
+              categoryFilter === null ? "border-2" : ""
+            }`}
+            onClick={() => handleCatFilter(null)}
+          >
+            All
+          </button>
+          {uniqueCategories.map((uniqueCategory, categoryIndex) => (
+            <button
+              key={categoryIndex}
+              onClick={() => handleCatFilter(uniqueCategory)}
+              className={`rounded-3xl border border-black border-solid inline-block bg-white mr-2 mb-4 px-3 py-1 ${
+                categoryFilter === uniqueCategory ? "border-2" : ""
+              }`}
+            >
+              {uniqueCategory}
+            </button>
           ))}
+        </div>
+        <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+          {filteredTickets.map((ticket, _index) => (
+            <TicketCard id={_index} key={_index} ticket={ticket} />
+          ))}
+        </div>
       </div>
     </main>
   );
