94624298c89309e77c5042aff0bca1906c93a2bf
Camilla Westin <camilla@webstin.se>
2023-11-11 12:54:19 +0100

    Framer motion to animate filter module

--- app/(components)/FilterModule.jsx
+++ app/(components)/FilterModule.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import FilterCheckbox from "@/app/(components)/FilterCheckbox";
+import { motion } from "framer-motion";
 
 const FilterModule = ({
   typeFilters,
   setTypeFilters,
   uniqueTypes,
   toggleTypeFilter,
+  variants,
 }) => {
   return (
-    <div className="mb-4 bg-white w-1/5 p-5 max-h-screen">
-      <label className="mb-4 font-semibold">Filter by type:</label>
-      <FilterCheckbox
-        label="All"
-        checked={typeFilters.length === 0}
-        onChange={() => setTypeFilters([])}
-      />
-      {uniqueTypes.map((uniqueType, typeIndex) => (
+    <div className="w-full p-5">
+      <motion.a variants={variants}>
+        <label className="mb-4 font-semibold">Filter by type:</label>
+      </motion.a>
+      <motion.a variants={variants}>
         <FilterCheckbox
-          key={typeIndex}
-          label={uniqueType}
-          checked={typeFilters.includes(uniqueType)}
-          onChange={() => toggleTypeFilter(uniqueType)}
+          label="All"
+          checked={typeFilters.length === 0}
+          onChange={() => setTypeFilters([])}
         />
-      ))}
+      </motion.a>
+      <motion.a variants={variants}>
+        {uniqueTypes.map((uniqueType, typeIndex) => (
+          <FilterCheckbox
+            key={typeIndex}
+            label={uniqueType}
+            checked={typeFilters.includes(uniqueType)}
+            onChange={() => toggleTypeFilter(uniqueType)}
+          />
+        ))}
+      </motion.a>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import getTickets from "@/app/api/Tickets/getTickets";
-import FilterCheckbox from "@/app/(components)/FilterCheckbox";
 import TicketColumn from "@/app/(components)/TicketColumn";
 import FilterModule from "./(components)/FilterModule";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { motion, useCycle } from "framer-motion";
 
 const Dashboard = () => {
   const [tickets, setTickets] = useState([]);
   const [typeFilters, setTypeFilters] = useState([]);
   const [uniqueTypes, setUniqueTypes] = useState([]);
+  const [openFilterModule, cycleFilterModule] = useCycle(false, true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +26,27 @@ const Dashboard = () => {
 
     fetchData();
   }, []);
+
+  const sideVariants = {
+    closed: {
+      transition: {
+        staggerChildren: 0.4,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        staggerChildren: 0.4,
+        staggerDirection: 1,
+      },
+    },
+  };
+  const itemVariants = {
+    closed: {
+      opacity: 0,
+    },
+    open: { opacity: 1 },
+  };
 
   function toggleTypeFilter(typ) {
     if (typeFilters.includes(typ)) {
@@ -45,15 +67,49 @@ const Dashboard = () => {
   return (
     <main>
       <div className="flex">
-        <FilterModule
-          typeFilters={typeFilters}
-          setTypeFilters={setTypeFilters}
-          uniqueTypes={uniqueTypes}
-          toggleTypeFilter={toggleTypeFilter}
-        />
-        <div className="w-4/5">
+        <motion.aside
+          initial={{
+            width: 0,
+            opacity: 0,
+            height: "100vh",
+            backgroundColor: "white",
+            left: "-350px",
+            position: "relative",
+          }}
+          animate={{
+            width: openFilterModule ? 350 : 0,
+            opacity: openFilterModule ? 1 : 0,
+            left: "0",
+          }}
+          exit={{
+            width: 0,
+            opacity: 0,
+            transition: { delay: 0.7, duration: 0.3 },
+          }}
+        >
+          <motion.div
+            className="container"
+            initial="closed"
+            animate={openFilterModule ? "open" : "closed"}
+            exit="closed"
+            variants={sideVariants}
+          >
+            <FilterModule
+              typeFilters={typeFilters}
+              setTypeFilters={setTypeFilters}
+              uniqueTypes={uniqueTypes}
+              toggleTypeFilter={toggleTypeFilter}
+              variants={itemVariants}
+            />
+          </motion.div>
+        </motion.aside>
+
+        <div className="">
           <div className="w-full h-14 block p-6">
-            <button className="bg-white py-2 px-3 rounded-3xl text-black flex items-center shadow">
+            <button
+              onClick={cycleFilterModule}
+              className="bg-white py-2 px-3 rounded-3xl text-black flex items-center shadow"
+            >
               <FontAwesomeIcon
                 icon={faFilter}
                 className="text-black text-xl mr-2"
